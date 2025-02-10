ff2d4f21b138cfeb714e7962b5dc169862ea2e35
nuaaxieqiang <xieqiang@nuaa.edu.cn>
2025-02-10 13:37:55 +0800

    测试修改2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Edit by Visual studio 2022 !");
+List<string> list = new List<string>();
+list.Add("Hello");
+list.Add("World");
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
